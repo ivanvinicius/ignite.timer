ecf43358d711dce67b557af33dc186b6f1a62300
ivanvinicius <ivanvinicius@github.com>
2022-10-06 15:00:06 -0300

    feat: bring better functionalities for inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,13 +16,26 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para sua tarefa" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para sua tarefa"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto do chevrolet cruze 2025" />
+            <option value="Votar no bolsonaro 2022" />
+            <option value="Assistir vídeos da finclass" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
