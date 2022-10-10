3c680a1cc2edc669a791e07ff481c88905c8948a
ivanvinicius <ivanvinicius@github.com>
2022-10-10 14:43:49 -0300

    feat: decreasing counter

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
 
 import {
   HomeContainer,
@@ -12,6 +15,13 @@ import {
   Separator,
   StartCountdownButton
 } from './styles'
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+}
 
 const newCycleSchemaValidation = zod.object({
   task: zod
@@ -27,6 +37,10 @@ const newCycleSchemaValidation = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleSchemaValidation>
 
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleID, setActiveCycleID] = useState<string | null>(null)
+  const [secondsGone, setSecondsGone] = useState(0)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleSchemaValidation),
     defaultValues: {
@@ -34,12 +48,40 @@ export function Home() {
       minutesAmount: 0
     }
   })
-  const isSubmitButtonDisabled = watch('task')
 
-  function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+  const isSubmitButtonDisabled = watch('task')
+  const activeCycle = cycles.find(
+    (currentCycle) => currentCycle.id === activeCycleID
+  )
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - secondsGone : 0
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+  const padMinutes = String(minutesAmount).padStart(2, '0')
+  const padSeconds = String(secondsAmount).padStart(2, '0')
+
+  function handleCreateNewCycle({ task, minutesAmount }: NewCycleFormData) {
+    const id = uuidv4()
+    const newCycle: Cycle = { id, task, minutesAmount, startDate: new Date() }
+
+    setCycles((currentState) => [...currentState, newCycle])
+    setActiveCycleID(id)
+
     reset()
   }
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        const difference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
+        )
+
+        setSecondsGone(difference)
+      }, 1000)
+    }
+  }, [activeCycle])
 
   return (
     <HomeContainer>
@@ -74,11 +116,11 @@ export function Home() {
         </FormContainer>
 
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{padMinutes[0]}</span>
+          <span>{padMinutes[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{padSeconds[0]}</span>
+          <span>{padSeconds[1]}</span>
         </CountdownContainer>
 
         <StartCountdownButton type="submit" disabled={!isSubmitButtonDisabled}>
